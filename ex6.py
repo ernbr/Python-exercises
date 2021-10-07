de298e4b75a3e84050d1d77db04108a65d0e7e23
ernbr <89361781+ernbr@users.noreply.github.com>
2021-10-06 21:56:00 -0300

    Create ex6.py
    '''#6 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área. '''

---
+++ ex6.py
@@ -0,0 +1,7 @@
+'''#6 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área. '''
+######################################## ########################################
+
+raio = float(input("Informe o raio de um circulo a ser calculado: "))
+print (f"A área aproximada do seu circulo é {raio**2*3.14:.2F}")
+
+######################################## ########################################
