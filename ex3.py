c56b91fbf69369ec78f85f9471f48cd3f0aa6c0e
ernbr <89361781+ernbr@users.noreply.github.com>
2021-10-06 21:53:40 -0300

    Create ex3.py
    '''#3 - Faça um Programa que peça dois números e imprima a soma. '''

---
+++ ex3.py
@@ -0,0 +1,9 @@
+'''#3 - Faça um Programa que peça dois números e imprima a soma. '''
+######################################## ########################################
+
+nu1 = int(input ("Informe um número"))
+nu2 = int(input ("Informe outro número"))
+soma = nu1 + nu2
+print (f'A soma do número foi {soma}')
+
+######################################## ########################################
