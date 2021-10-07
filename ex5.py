e8090f22e3811b7ee9e920d0ae10eb31e0226822
ernbr <89361781+ernbr@users.noreply.github.com>
2021-10-06 21:55:27 -0300

    Create ex5.py
    '''#5 - Faça um Programa que converta metros para centímetros. '''

---
+++ ex5.py
@@ -0,0 +1,7 @@
+'''#5 - Faça um Programa que converta metros para centímetros. '''
+######################################## ########################################
+
+metros = float(input("Informe a metragem a ser convertida: "))
+print (f"O seu valor em centimetros é {metros*100}")
+
+######################################## ########################################
