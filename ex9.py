966d82ce6b3edafb09d96febeb58f2a0607e17b0
ernbr <89361781+ernbr@users.noreply.github.com>
2021-10-06 21:57:58 -0300

    Create ex9.py
    '''#9 - Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.		
    C = 5 * ((F-32) / 9). '''

---
+++ ex9.py
@@ -0,0 +1,9 @@
+'''#9 - Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.		
+C = 5 * ((F-32) / 9). '''
+######################################## ########################################
+
+temp_f = float(input ("Informe a temperatura em graus Fahrenheit a ser convertida em Celsius: "))
+
+print (f'A temperatura em Celsius é {5*(temp_f-32)/9:.2f}')
+
+######################################## ########################################
