90c8748f266b41932643caec1e45f12322ea3dec
ernbr <89361781+ernbr@users.noreply.github.com>
2021-10-06 21:58:37 -0300

    Create ex10.py
    '''#10 - Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit. '''

---
+++ ex10.py
@@ -0,0 +1,8 @@
+'''#10 - Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit. '''
+######################################## ########################################
+
+temp_c = float(input ("Informe a temperatura em graus Celsius a ser convertida em Fahrenheit: "))
+
+print (f'A temperatura em Fahrenheit é {temp_c*9/5 +32:.2f}')
+
+######################################## ########################################
