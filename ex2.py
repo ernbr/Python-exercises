d111af84e06195a871216a337c02a92c41dbbef4
ernbr <89361781+ernbr@users.noreply.github.com>
2021-10-06 21:52:58 -0300

    Create ex2.py
    ''' #2 - Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. '''

---
+++ ex2.py
@@ -0,0 +1,6 @@
+''' #2 - Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. '''
+######################################## ########################################
+print ("Informe um número de 0 a 10")
+msg_user = int(input (""))
+print (f'O seu número foi {msg_user}')
+######################################## ########################################
