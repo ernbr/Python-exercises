30b956ec01cd274b56fc9c46ec1449b3e5f7c501
ernbr <89361781+ernbr@users.noreply.github.com>
2021-10-06 21:59:42 -0300

    Create ex11.py
    '''#11 - Faça um Programa que peça 2 números inteiros e um número real. 
        Calcule e mostre: 
        O produto do dobro do primeiro com metade do segundo.
        A soma do triplo do primeiro com o terceiro.
        O terceiro elevado ao cubo. '''

---
+++ ex11.py
@@ -0,0 +1,18 @@
+'''#11 - Faça um Programa que peça 2 números inteiros e um número real. 
+    Calcule e mostre: 
+    O produto do dobro do primeiro com metade do segundo.
+    A soma do triplo do primeiro com o terceiro.
+    O terceiro elevado ao cubo. '''
+
+######################################## ########################################
+
+nu1 = float(input ("Informe um numero inteiro: "))
+nu2 = float(input ("Informe outro numero inteiro: "))
+nur = float(input ("informe um número real"))
+
+print (f'O produto do dobro do primeiro com metade do segundo é igual a : {2*nu1+(nu2/2)}')
+print (f'A soma do triplo do primeiro com o terceiro é igual a: {(3*nu1)+nur}')
+print (f'o terceiro valor elevado ao cubo é igual a:  {nur**3}')
+
+######################################## ########################################
+    
