494ed92b5976fd782dce73911af97a0da3e35808
ernbr <89361781+ernbr@users.noreply.github.com>
2021-10-06 21:57:14 -0300

    Create ex8.py
    '''#8 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
    Calcule e mostre o total do seu salário no referido mês. '''

---
+++ ex8.py
@@ -0,0 +1,10 @@
+'''#8 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
+Calcule e mostre o total do seu salário no referido mês. '''
+######################################## ########################################
+
+ho1 = int(input ("Informe o seu salário por hora: "))
+ho2 = int(input ("Informe o total de horas trabalhadas no mês: "))
+
+print (f'O total pago no mês foi de {ho1*ho2}')
+
+######################################## ########################################
