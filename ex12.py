4dc68fb020f1489286a5c896514905d7365dd834
ernbr <89361781+ernbr@users.noreply.github.com>
2021-10-06 22:00:30 -0300

    Create ex12.py
    '''#12 - Tendo como dados de entrada a altura de uma pessoa, 
    construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58'''

---
+++ ex12.py
@@ -0,0 +1,9 @@
+'''#12 - Tendo como dados de entrada a altura de uma pessoa, 
+construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58'''
+######################################## ########################################
+
+altura = float(input ("Informe a sua altura: "))
+
+print (f'O seu peso ideal de acordo com a sua altura é {(72.7*altura)-58 :.2f}')
+
+######################################## ########################################
