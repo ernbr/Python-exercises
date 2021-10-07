1c5956b4602933c2b12e45fdafb83ff0ea259060
ernbr <89361781+ernbr@users.noreply.github.com>
2021-10-06 21:56:35 -0300

    Create ex7.py
    '''#7 - Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. '''

---
+++ ex7.py
@@ -0,0 +1,8 @@
+'''#7 - Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. '''
+######################################## ########################################
+
+lado = float(input("Informe o comprimento de um lado do seu quadrado: "))
+print (f"A área total do seu quadrado é {lado*4:.2F}")
+print (f"O dobro da área total do seu quadrado é {lado*4*2:.2F}")
+
+######################################## ########################################
