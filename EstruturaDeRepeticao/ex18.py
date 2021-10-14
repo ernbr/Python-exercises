563561da365059d424ccdfdaf8c481b33a32b775
Emerson Santos <89361781+ernbr@users.noreply.github.com>
2021-10-14 01:32:18 -0300

    Create ex18.py
    '''
    #18 -Faça um programa que, dado um conjunto de N números, determine o menor valor, o maior valor e a soma dos valores.
    '''

---
+++ EstruturaDeRepeticao/ex18.py
@@ -0,0 +1,23 @@
+'''
+#18 -Faça um programa que, dado um conjunto de N números, determine o menor valor, o maior valor e a soma dos valores.
+'''
+total=int(input('Informe uma quantidade de numero: '))
+
+numeros=1
+maior=1
+menor=100
+soma=0
+while numeros <= total:
+        digite = int(input('Informe o numero %d:'%numeros))
+        numeros+=1
+        if digite > maior:
+            maior=digite
+
+        if digite <menor:
+            menor=digite
+
+        soma=soma+digite
+
+print(f'O seu maior numero foi {maior}')
+print(f'O seu menor numero foi {menor}')
+print(f'A soma dos numero foi {soma}')
