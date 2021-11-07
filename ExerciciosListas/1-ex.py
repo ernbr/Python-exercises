a313902b843de56ac82d3192e5f475dcedbfcbbf
Emerson Santos <89361781+ernbr@users.noreply.github.com>
2021-11-07 18:53:46 -0300

    Create 1-ex.py
    '''
    #1 - Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
    '''

---
+++ ExerciciosListas/1-ex.py
@@ -0,0 +1,10 @@
+'''
+#1 - Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+'''
+
+lista_vetor = []
+
+while len(lista_vetor) != 5:
+    lista_vetor.append(int(input('Informe um numero: ')))
+
+print(lista_vetor)
